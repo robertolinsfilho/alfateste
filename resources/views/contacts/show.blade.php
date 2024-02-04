0bbdefd835a287793e1d5a3b52c650ca08d78860
roberto <rlins74@gmail.com>
2024-02-04 14:44:24 -0300

    alteração

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -10,27 +10,6 @@
         </nav>
     </div>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Nome:</strong>
-                {{ $contact->name }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                {{ $contact->email }}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Contato:</strong>
-                {{ $contact->contact }}
-            </div>
-        </div>
-    </div>
-
 
     <table class="table table-striped">
         <tr>
